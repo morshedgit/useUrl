a446b2a8670bda9fc6ce0af60437847c76562dbf
Sadeq Morshedloo <smorshedloo@cercacor.com>
2023-11-24 18:14:51 -0800

    Adds sign in button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { NextPageContext } from "next";
 import Link from "next/link";
 
-export default async function Home(context: NextPageContext) {
-  const shop = context.query.shop;
+export default function Home({ searchParams: { shop } }: any) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Welcome
-      <Link href={`/api/install?shop${shop}`}>Sign In</Link>
+      <Link href={`/api/install?shop=${shop}`}>Sign In</Link>
     </main>
   );
 }
