e121e6f0c407bd4d06065bf92ec03d6ae48ca7c4
Sadeq Morshedloo <smorshedloo@cercacor.com>
2023-11-24 16:01:06 -0800

    Adds test route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,7 +2,11 @@ export async function GET(request: Request) {
   // Extract the code from the query parameters
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
-  console.log("Reached");
+  console.log("Reached: ", code);
 
-  return Response.redirect(`/?${code}`);
+  // Ensure to include the protocol in the redirect URL
+  const redirectUrl = new URL(`${url.protocol}//${url.host}`);
+  redirectUrl.pathname = "/"; // if you have a specific path
+  redirectUrl.searchParams.append("code", code || "null");
+  return Response.redirect(redirectUrl.toString());
 }
